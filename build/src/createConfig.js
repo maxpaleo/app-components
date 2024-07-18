e254e0b3b0de3267e3a82f77f06682d04e8cd25a
Max Pauwels <91980500+maxpaleo@users.noreply.github.com>
2024-07-18 00:50:40 -0700

    Npm update

---
+++ build/src/createConfig.js
@@ -0,0 +1,36 @@
+// const fs = require("fs");
+// const path = require("path");
+// const configContent = `// Configuration
+// export const config = {
+//   key: 'value',
+//   // Add more configuration options as needed
+// };`;
+// const configPath = path.join(process.cwd(), "app-config.ts");
+// fs.writeFile(configPath, configContent, { flag: "wx" }, (err) => {
+//   if (err) {
+//     if (err.code === "EEXIST") {
+//       console.error("app-config.ts already exists. No new file was created.");
+//     } else {
+//       console.error(err);
+//     }
+//   } else {
+//     console.log("app-config.ts has been created.");
+//   }
+// });
+import * as fs from "fs";
+import * as path from "path";
+var configContent = "// Configuration\nexport const config = {\n  key: 'value',\n  // Add more configuration options as needed\n};";
+var configPath = path.join(process.cwd(), "app-config.ts");
+fs.writeFile(configPath, configContent, { flag: "wx" }, function (err) {
+    if (err) {
+        if (err.code === "EEXIST") {
+            console.error("app-config.ts already exists. No new file was created.");
+        }
+        else {
+            console.error(err);
+        }
+    }
+    else {
+        console.log("app-config.ts has been created.");
+    }
+});
---
+++ src/createConfig.ts
@@ -0,0 +1,50 @@
+// const fs = require("fs");
+// const path = require("path");
+
+// const configContent = `// Configuration
+// export const config = {
+//   key: 'value',
+//   // Add more configuration options as needed
+// };`;
+
+// const configPath = path.join(process.cwd(), "app-config.ts");
+
+// fs.writeFile(configPath, configContent, { flag: "wx" }, (err) => {
+//   if (err) {
+//     if (err.code === "EEXIST") {
+//       console.error("app-config.ts already exists. No new file was created.");
+//     } else {
+//       console.error(err);
+//     }
+//   } else {
+//     console.log("app-config.ts has been created.");
+//   }
+// });
+
+import * as fs from "fs";
+import * as path from "path";
+
+const configContent: string = `// Configuration
+export const config = {
+  key: 'value',
+  // Add more configuration options as needed
+};`;
+
+const configPath: string = path.join(process.cwd(), "app-config.ts");
+
+fs.writeFile(
+  configPath,
+  configContent,
+  { flag: "wx" },
+  (err: NodeJS.ErrnoException | null) => {
+    if (err) {
+      if (err.code === "EEXIST") {
+        console.error("app-config.ts already exists. No new file was created.");
+      } else {
+        console.error(err);
+      }
+    } else {
+      console.log("app-config.ts has been created.");
+    }
+  }
+);
