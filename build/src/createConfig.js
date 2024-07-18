6c3e5fb22766aa94086910cff8e27aa359aeeee0
Max Pauwels <91980500+maxpaleo@users.noreply.github.com>
2024-07-18 00:53:03 -0700

    Npm update

--- build/src/createConfig.js
+++ build/src/createConfig.js
@@ -19,18 +19,18 @@
 // });
 import * as fs from "fs";
 import * as path from "path";
-var configContent = "// Configuration\nexport const config = {\n  key: 'value',\n  // Add more configuration options as needed\n};";
+var configContent =
+  "// Configuration\nexport const config = {\n  key: 'value',\n  // Add more configuration options as needed\n};";
 var configPath = path.join(process.cwd(), "app-config.ts");
 fs.writeFile(configPath, configContent, { flag: "wx" }, function (err) {
-    if (err) {
-        if (err.code === "EEXIST") {
-            console.error("app-config.ts already exists. No new file was created.");
-        }
-        else {
-            console.error(err);
-        }
+  if (err) {
+    if (err.code === "EEXIST") {
+      console.error("app-config.ts already exists. No new file was created.");
+    } else {
+      console.error(err);
     }
-    else {
-        console.log("app-config.ts has been created.");
-    }
+  } else {
+    console.log("app-config.ts has been created.");
+  }
 });
+exports.default = createConfig;
